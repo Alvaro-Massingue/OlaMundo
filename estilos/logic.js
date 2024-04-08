da72965f22922957a3f5d1bd8c0cc319e22765c5
Alvaro Massingue <alvaroruimassingue658@gmail.com>
2024-04-08 16:26:48 +0100

    Mudanças na responsividade
    Fiz algumas mudanças na página emprol da melhoria na responsividade

--- estilos/logic.js
+++ estilos/logic.js
@@ -1,3 +1,5 @@
+window.alert("Olá! Eu sou Alvaro. Gostaria de informar que essa página ainda está em desenvolvimento, portanto espero pelo seu feedback")
+
 window.addEventListener("scroll", function(){
 
     var header =document.querySelector("header");
